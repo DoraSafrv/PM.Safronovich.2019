23ccff141a6e94f54b24f961b8549b73c9513583
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2019-09-29 14:56:49 +0300

    треугольник

---
+++ 1 семестр/29.09.2019.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	char ok = 'y';
+
+	while (ok == 'y')
+	{
+		while (true)
+		{
+			cout << "Enter the side lenghts.\n";
+			cin >> a;
+			cin >> b;
+			cin >> c;
+
+			if ((a + b > c || a + c > b || b + c > a))
+			{
+				break;
+			}
+			
+			else
+			{
+				cout << "Not triangle.\n";
+				system("pause");
+				system("cls");
+			}
+		}
+		
+		if (a == b && b == c)
+		{
+			cout << "Triangle is equilateral.\n";
+		}
+		
+		else if (a == b || a == c || c == b)
+		{
+			cout << "Triangle is isosceles.\n";
+		}
+
+		else
+		{
+			cout << "Triangle versatile.\n";
+		}
+
+		cout << "Press 'y' to continue.\n";
+		cin >> ok;
+		system("pause");
+		system("cls");
+	}
+
+	return 0;
+}
+
