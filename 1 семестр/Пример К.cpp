6d86e5177efcde939a6fdc17d84f5ed2c19b9302
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2019-10-02 00:42:51 +0300

    Create Пример К.cpp

---
+++ 1 семестр/Пример К.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double sum = 0.0, denominator = 0.0;
+
+	int n;
+
+	while (true)
+	{
+		cout << "n = ";
+		cin >> n;
+
+		if (n<=0)
+		{
+			cout << "N > 0! \n";
+			system("pause");
+			system("cls");
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+	int i = 1;
+
+	while (i <= n)
+	{
+		denominator += sin(i);
+		sum += 1 / denominator;
+
+		i++;
+	}
+
+	cout << "sum = " << sum;
+
+	system("pause");
+	return 0;
+}
