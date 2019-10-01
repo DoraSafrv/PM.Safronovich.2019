5bfb5bcea00517182dfd590bd1f97b1d406b7909
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2019-10-02 00:35:38 +0300

    Create Пример И.cpp

---
+++ 1 семестр/Пример И.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double sum = 0;
+
+	int n;
+
+	while (true)
+	{
+		cout << "n = ";
+		cin >> n;
+
+		if (n <= 0)
+		{
+			cout << "N>0! \n";
+			system("pause");
+			system("cls");
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	int i = 1.0;
+
+	while (i <= n)
+	{
+		sum = sqrt(2 + sum);
+
+		i++;
+	}
+
+	cout << "sum = " << sum;
+
+	system("pause");
+	return 0;
+}
