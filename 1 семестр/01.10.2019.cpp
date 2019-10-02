3eb805faffe88449d01b69428ce119e1b818ec50
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2019-10-02 09:53:50 +0300

    Create 01.10.2019.cpp

---
+++ 1 семестр/01.10.2019.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x_point, y_point;
+
+	cout << "Enter x";
+	cin >> x;
+	cout << "Enter y";
+	cin >> y;
+
+	if (x > 0 && pow(x, 2) + pow(y, 2) <= 1)
+	{
+		cout << "The point belongs to the area";
+	}
+	else if (x<0 && x>= -1 && y >= -1 && y <= 1)
+	{
+		cout << "The point belongs to the area";
+	}
+	else
+	{
+		cout << "The point does not belongs to the area";
+	}
+	system("pause");
+	return 0;
+}
+
