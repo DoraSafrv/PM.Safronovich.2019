12a5001fa2704ddf0d9d4c70475af91976c50458
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2019-10-22 23:46:37 +0300

    .

---
+++ 1 семестр/16.10.2019/4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int size(int a)
+{
+	int size_ = 0;
+
+	while (a > 0)
+	{
+		size_++;
+		a /= 10;
+	}
+
+	return size_;
+}
+
+void doRa(int n, int was, int became)
+{
+	while (n > 0)
+	{
+		int buffer = n / (int)pow(10, size(n) - 1);
+		n %= (int)pow(10, size(n) - 1);
+
+		if (buffer == was)
+		{
+			cout << became;
+		}
+		else
+		{
+			cout << buffer;
+		}
+	}
+}
+
+int main()
+{
+	int n, was, became;
+	cin >> n >> was >> became;
+
+	doRa(n, was, became);
+
+	system("pause");
+	return 0;
+}
---
+++ 1 семестр/16.10.2019/5.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int size(int a)
+{
+	int size_ = 0;
+
+	while (a > 0)
+	{
+		size_++;
+		a /= 10;
+	}
+
+	return size_;
+}
+
+void doRa(int n, int del)
+{
+	while (n > 0)
+	{
+		int buffer = n / (int)pow(10, size(n) - 1);
+		n %= (int)pow(10, size(n) - 1);
+
+		if (buffer != del)
+		{
+			cout << buffer;
+		}
+	}
+}
+
+int main()
+{
+	int n, del;
+	cin >> n >> del;
+
+	doRa(n, del);
+
+	system("pause");
+	return 0;
+}
