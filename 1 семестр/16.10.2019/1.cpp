49597ed0fd5921d051971975d5ab39aecd12dfbb
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2019-10-22 23:37:26 +0300

    .

---
+++ 1 семестр/16.10.2019/1.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <math.h>
+#include "MenuFunctions.h"
+
+using namespace std;
+void menu();
+void workWithConsole();
+int primeNumber();
+void primeNumberTests(int, char);
+
+int main()
+{
+	menu();
+	return 0;
+
+}
+
+void menu()
+{
+	while (true)
+	{
+		char operation;
+		menuText();
+		cin >> operation;
+		switch (operation)
+		{
+		case '1':
+			workWithConsole();
+			break;
+		case '2':
+			runTests();
+			break;
+		case '3':
+			return;
+		default:
+			cout << "Invalid operation. Try again." << endl;
+		}
+		system("pause");
+		system("cls");
+	}
+}
+
+void workWithConsole()
+{
+	while (true)
+	{
+		int number;
+
+		cout << "Please enter number = ";
+		cin >> number;
+
+		if (number < 1)
+		{
+			cout << "Please enter number > 1!";
+			break;
+		}
+		primeNumber(number);
+	}
+}
+
+int primeNumber(int number)
+{
+	
+	for (int i = 2; i <= sqrt(number); i++)
+	{
+		if (number % i == 0)
+		{
+			cout << "Not Prime" << endl;
+			char notprime; 
+			return 0;
+		}
+	}
+	char prime;
+	cout << "Prime" << endl;
+	system("pause");
+	return 0;
+}
+
+void runTests()
+{
+	int testNumber = 1;
+
+	primeNumberTests (testNumber++, 13, char );
+	
+}
+
+void primeNumberTests(int number)
+{
+	int number = 2;
+	char (prime);
+
+	if (actual1 == expected1 && actual2 == expected2 && actual3 == expected3)
+	{
+		cout << "Case #" << testCaseNumber << " is correct. :)" << endl;
+	}
+	else
+	{
+		cout << "Case #" << testCaseNumber << " IS NOT CORRECT. :(" << endl;
+		cout << "\t actual = " << actual << ", but expected = " << expectedCount << endl;
+	}
+}
---
+++ 1 семестр/16.10.2019/2.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int enterNumber();
+void displayResult(int, short, int);
+void countTests(int, int, short, short, short);
+void menu();
+void workWithConsole();
+void runTests();
+void menuText();
+
+
+int main()
+{
+	menu();
+	return 0;
+}
+
+void menu()
+{
+	while (true)
+	{
+		char operation;
+		menuText();
+		cin >> operation;
+		switch (operation)
+		{
+		case '1':
+			workWithConsole();
+			break;
+		case '2':
+			runTests();
+			break;
+		case '3':
+			return;
+		default:
+			cout << "Invalid operation. Try again." << endl;
+		}
+		system("pause");
+		system("cls");
+	}
+}
+
+void menuText()
+{
+	cout << "Press 1 to work with the console." << endl;
+	cout << "Press 2 to run tests." << endl;
+	cout << "Press 3 to exit." << endl;
+}
+
+void workWithConsole()
+{
+	int number = enterNumber();
+
+	if (number == reverse(number))
+		cout << "True";
+	else
+		cout << "False";
+
+	system("pause");
+	return 0;
+}
+
+int enterNumber()
+{
+	int number;
+	cout << "number = "; 
+	cin >> number;
+
+	return number;
+}
+
+int reverse(int number)
+{
+	int buffer = 0;
+
+	while (number > 0)
+	{
+		buffer = buffer * 10 + number % 10;
+		number /= 10;
+	}
+
+	return buffer;
+}
+
+void runTests()
+{
+	int testNumber = 1;
+
+	countTests(testNumber++, );
+	countTests(testNumber++,);
+
+}
+
+void countTests()
+{
+
+	if ()
+	{
+		cout << "Case #" << testCaseNumber << " is correct. :)" << endl;
+	}
+	else
+	{
+		cout << "Case #" << testCaseNumber << " IS NOT CORRECT. :(" << endl;
+		//cout << "\t actual = " << actual << ", but expected = " << expectedCount << endl;
+	}
+}
