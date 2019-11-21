609a016cf0ce8e1775bbd560d6040a9b165dfe59
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2019-11-21 23:07:29 +0300

    finished

---
+++ 1 семестр/PM.Safronovich.06.10.2019/06.10.19.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+
+void change(int* ptr, int num) {     // 1 task
+	int count = 0;
+	int temp;
+	while (count < num) {
+		temp = *(ptr + count);
+		*(ptr + count) = *(ptr + num - 1);
+		*(ptr + num - 1) = temp;
+		count++;
+		num--;
+	}
+}
+
+
+
+bool isPrime(int n)                                  // 2 task
+{
+	if (n > 1){
+		for (int i = 2; i < n; i++)
+			if (n % i == 0) {
+				return false;
+			}
+		return true;
+	} else {
+		return false;
+	}
+}
+
+int* prime(int* ptr, int num, int* el) {
+	int* arr2 = new int[num];
+	int count = 0;
+	for (int i = 0; i < 0; i++) {
+		if (isPrime(*(ptr + i))) {
+			arr2[count] = *(ptr + i);
+		}
+	}
+	*(el) = count + 1;
+}
+
+
+
+int num_of_el(int* ptr, int num, int new_num) {    //3 task
+	int count = 0;
+	for (int i = 0; i < num; i++) {
+		if (*(ptr + i) < new_num) {
+			count++;
+		}
+		else {
+			return count;
+		}
+	}
+	return count;
+}
+
+
+
+void typeOfSequence(int* ptr, int num) {   //4 task
+	int increase = 0;
+	int decreasing = 0;
+	int not_increase = 0;
+	int not_decreasing = 0;
+	for (int i = 1; i < num; i++) {
+		if (*(ptr + i - 1) > * (ptr + i)) {
+			decreasing++;
+		}
+		if (*(ptr + i - 1) < * (ptr + i)) {
+			increasing++;
+		}
+		if (*(ptr + i - 1) >= * (ptr + i)) {
+			not_increasing++;
+		}
+		if (*(ptr + i - 1) < *(ptr + i )) {
+			not_decreasing++;
+		}
+	}
+	if (not_decreasing == num) {
+		std::cout << "function does not increase ";
+	} 
+	else if (not_increasing == num) {
+		std::cout << "function does not increases ";
+	} 
+	else if (increasing == num) {
+		std::cout << "function increases ";
+	} 
+	else if(decreasing == num) {
+		std::cout << "function decreases ";
+	}
+	else {
+		std::cout << "not categorized";
+	}
+}
+
+
+
+void output(int* ptr, int num) {
+	for (int i = 0; i < num; i++) {
+		std::cout << *(ptr + i) << "\t";
+	}
+}
+
+
+
+int main() {
+	int num;
+	std::cout << "Enter integer value: ";
+	std::cin >> num;
+
+	int* arr = new int[num];
+	for (int i = 0; i < num; i++) {
+		std::cin >> arr[i];
+	}
+
+	typeOfSequence(arr, num);
+
+	output(arr, num);
+}
