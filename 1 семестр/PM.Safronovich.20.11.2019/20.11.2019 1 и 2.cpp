fcaa64def303daf5800052693991cb188a0b050e
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2019-12-17 23:20:13 +0300

    .

---
+++ 1 семестр/PM.Safronovich.20.11.2019/20.11.2019 1 и 2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+
+void equals(const char* lhs, int n, const char* rhs, int m);
+void display(char* lhs, int n);
+
+int main()
+{
+	int num;
+	cin >> num;
+
+	char* lhs = new char[num];
+	for (int i = 0; i < num; i++)
+	{
+		cin >> lhs[i];
+	}
+
+	int num1;
+	cin >> num1;
+	char* rhs = new char[num1];
+	for (int i = 0; i < num1; i++) 
+	{
+		cin >> rhs[i];
+	}
+	equals(lhs, num, rhs, num1);
+}
+
+
+void display(char* rhs, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << rhs[i] << " ";
+	}
+}
+
+void equals(const char* lhs, int num, const char* rhs, int num1)
+{
+	int count = 0;
+	if (num1 == num) 
+	{
+		for (int i = 0; i < num; i++) 
+		{
+			if (lhs[i] == rhs[i]) 
+			{
+				count++;
+			}
+		}
+		if (num == count)
+		{
+			cout << "Arrays are equals";
+		}
+		else
+		{
+			cout << "Arrays are not equals";
+		}
+	}
+	else if (num > num1)
+	{
+		cout << "lhs > rhs";
+	}
+	else if (num1 > num) 
+	{
+		cout << "lhs < rhs";
+	}
+
+}
