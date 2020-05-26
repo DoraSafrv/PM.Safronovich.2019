84ffd3f7f4aa7a497af82484a5c716fce49ee176
DoraSafrv <55157754+DoraSafrv@users.noreply.github.com>
2020-05-26 15:36:53 +0300

    Update Task.cpp

--- 2 семестр/Matrix/Task.cpp
+++ 2 семестр/Matrix/Task.cpp
@@ -4,9 +4,6 @@
 
 using namespace std;
 
-void matrixUrod(Matrix&);
-int findElement(Matrix&, int, int);
-
 int main()
 {
 const char* path = "file.txt";
@@ -14,18 +11,5 @@ ifstream a(path);
 Matrix b(4,4);
 a>>b;
 cout << b;
-
 }
-
-void matrixUrod(Matrix& matrix)
-{
-	for (int i = 0; i < matrix.getRows(); i++)
-	{
-		for (int j = 0; j < matrix.getColumns; j++)
-		{
-			matrix[i][j] = // сортировка матрицы оч непанятна
-		}
-	}
-}
-
 
